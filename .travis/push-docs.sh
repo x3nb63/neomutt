3676dbe5079179cb67d3d5381e70828e1090638a
James Vaughan <james@jamesbvaughan.com>
2018-05-11 14:37:15 -0700

    add script for deploying code docs

---
+++ .travis/push-docs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# exit early if any command fails
+set -e
+
+git clone --recursive https://github.com/neomutt/neomutt.github.io
+
+(
+cd ../neomutt.github.io/doxygen
+
+# Remove the old docs
+git rm -r ./*
+
+# Add the newly generated docs
+cp -r ../../html/* ./
+git commit --all -m "Update docs"
+git push origin master
+)
+
+git commit --all -m "Updated docs submodule"
