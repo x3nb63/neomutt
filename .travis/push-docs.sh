64a97ba74568d8286e520182025c6b11d582d5ed
Richard Russon <rich@flatcap.org>
2018-05-15 15:03:44 +0100

    add deployment keys

--- .travis/push-docs.sh
+++ .travis/push-docs.sh
@@ -3,6 +3,11 @@
 # exit early if any command fails
 set -e
 
+openssl aes-256-cbc -K $encrypted_3ed0348c37f7_key -iv $encrypted_3ed0348c37f7_iv -in .travis/doxygen.tar.enc -out .travis/doxygen.tar -d
+tar xf .travis/doxygen.tar
+head travis-deploy-doxygen
+head travis-deploy-website
+
 git clone --recursive https://github.com/neomutt/neomutt.github.io
 
 (
