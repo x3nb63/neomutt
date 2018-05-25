b6ed5fb062c57c79eb09c3706baa3754ff1ace35
Pietro Cerutti <gahr@gahr.ch>
2018-05-25 11:22:49 +0000

    Reduce diff

--- init.c
+++ init.c
@@ -3688,11 +3688,10 @@ void mutt_free_opts(void)
   mutt_regexlist_free(&UnMailLists);
   mutt_regexlist_free(&UnSubscribedLists);
 
+  mutt_groups_free();
   mutt_hash_destroy(&ReverseAliases);
   mutt_hash_destroy(&TagFormats);
   mutt_hash_destroy(&TagTransforms);
-
-  mutt_groups_free();
 
   /* Lists of strings */
   mutt_list_free(&AlternativeOrderList);
@@ -3765,7 +3764,6 @@ int mutt_init(bool skip_sys_rc, struct ListHead *commands)
   err.dptr = err.data;
 
   mutt_groups_init();
-
   /* reverse alias keys need to be strdup'ed because of idna conversions */
   ReverseAliases = mutt_hash_create(
       1031, MUTT_HASH_STRCASECMP | MUTT_HASH_STRDUP_KEYS | MUTT_HASH_ALLOW_DUPS);
