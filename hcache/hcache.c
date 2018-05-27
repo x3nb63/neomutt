c5a5561019a10749a97a6361aec0a7a73e5e4064
Bo YU <yuzibode@126.com>
2018-05-27 21:17:40 +0800

    triavs: lua
    Maybe i disable --lua, so my local make is ok.It is not to check
    hcache/hcache.o
    
    Signed-off-by: Bo YU <yuzibode@126.com>

--- hcache/hcache.c
+++ hcache/hcache.c
@@ -912,7 +912,7 @@ header_cache_t *mutt_hcache_open(const char *path, const char *folder, hcache_na
     } digest;
     struct Md5Ctx ctx;
     struct ReplaceList *spam = NULL;
-    struct RegexList *nospam = STAILQ_FIRST(NoSpamList), *next = NULL;
+    struct RegexListNode *nospam = STAILQ_FIRST(NoSpamList), *next = NULL;
 
     hcachever = HCACHEVER;
 
@@ -935,7 +935,7 @@ header_cache_t *mutt_hcache_open(const char *path, const char *folder, hcache_na
     }
     */
     /* Ha, stailq breaks silent here */
-    STAILQ_FOREACH(np, NoSpamList, entries)
+    STAILQ_FOREACH(nospam, NoSpamList, entries)
     {
       mutt_md5_process(nospam->regex->pattern, &ctx);
 
